41bbb0c6c18c0bacbae06ab4527385a3344976a0
Paulo de Almeida <paulodealmeida@gmail.com>
2018-10-30 10:49:20 -0300

    Removida a chamada de State
    Removida a chamada de State para deixar o seed independente.

--- StatesTableSeeder.php
+++ StatesTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\State;
 
 class StatesTableSeeder extends Seeder
 {
@@ -15,33 +14,33 @@ class StatesTableSeeder extends Seeder
     {
         DB::table('states')->delete();
 
-        State::create(['id' => 1, 'name' => 'Acre', 'abbreviation' => 'AC']);
-        State::create(['id' => 2, 'name' => 'Alagoas', 'abbreviation' => 'AL']);
-        State::create(['id' => 3, 'name' => 'Amapá', 'abbreviation' => 'AP']);
-        State::create(['id' => 4, 'name' => 'Amazonas', 'abbreviation' => 'AM']);
-        State::create(['id' => 5, 'name' => 'Bahia', 'abbreviation' => 'BA']);
-        State::create(['id' => 6, 'name' => 'Ceará', 'abbreviation' => 'CE']);
-        State::create(['id' => 7, 'name' => 'Distrito Federal', 'abbreviation' => 'DF']);
-        State::create(['id' => 8, 'name' => 'Espírito Santo', 'abbreviation' => 'ES']);
-        State::create(['id' => 9, 'name' => 'Goiás', 'abbreviation' => 'GO']);
-        State::create(['id' => 10, 'name' => 'Maranhão', 'abbreviation' => 'MA']);
-        State::create(['id' => 11, 'name' => 'Mato Grosso', 'abbreviation' => 'MT']);
-        State::create(['id' => 12, 'name' => 'Mato Grosso do Sul', 'abbreviation' => 'MS']);
-        State::create(['id' => 13, 'name' => 'Minas Gerais', 'abbreviation' => 'MG']);
-        State::create(['id' => 14, 'name' => 'Pará', 'abbreviation' => 'PA']);
-        State::create(['id' => 15, 'name' => 'Paraíba', 'abbreviation' => 'PB']);
-        State::create(['id' => 16, 'name' => 'Paraná', 'abbreviation' => 'PR']);
-        State::create(['id' => 17, 'name' => 'Pernambuco', 'abbreviation' => 'PE']);
-        State::create(['id' => 18, 'name' => 'Piauí', 'abbreviation' => 'PI']);
-        State::create(['id' => 19, 'name' => 'Rio de Janeiro', 'abbreviation' => 'RJ']);
-        State::create(['id' => 20, 'name' => 'Rio Grande do Norte', 'abbreviation' => 'RN']);
-        State::create(['id' => 21, 'name' => 'Rio Grande do Sul', 'abbreviation' => 'RS']);
-        State::create(['id' => 22, 'name' => 'Rondônia', 'abbreviation' => 'RO']);
-        State::create(['id' => 23, 'name' => 'Roraima', 'abbreviation' => 'RR']);
-        State::create(['id' => 24, 'name' => 'Santa Catarina', 'abbreviation' => 'SC']);
-        State::create(['id' => 25, 'name' => 'São Paulo', 'abbreviation' => 'SP']);
-        State::create(['id' => 26, 'name' => 'Sergipe', 'abbreviation' => 'SE']);
-        State::create(['id' => 27, 'name' => 'Tocantins', 'abbreviation' => 'TO']);
+        DB::table('states')->insert(['id' => 1, 'name' => 'Acre', 'abbreviation' => 'AC']);
+        DB::table('states')->insert(['id' => 2, 'name' => 'Alagoas', 'abbreviation' => 'AL']);
+        DB::table('states')->insert(['id' => 3, 'name' => 'Amapá', 'abbreviation' => 'AP']);
+        DB::table('states')->insert(['id' => 4, 'name' => 'Amazonas', 'abbreviation' => 'AM']);
+        DB::table('states')->insert(['id' => 5, 'name' => 'Bahia', 'abbreviation' => 'BA']);
+        DB::table('states')->insert(['id' => 6, 'name' => 'Ceará', 'abbreviation' => 'CE']);
+        DB::table('states')->insert(['id' => 7, 'name' => 'Distrito Federal', 'abbreviation' => 'DF']);
+        DB::table('states')->insert(['id' => 8, 'name' => 'Espírito Santo', 'abbreviation' => 'ES']);
+        DB::table('states')->insert(['id' => 9, 'name' => 'Goiás', 'abbreviation' => 'GO']);
+        DB::table('states')->insert(['id' => 10, 'name' => 'Maranhão', 'abbreviation' => 'MA']);
+        DB::table('states')->insert(['id' => 11, 'name' => 'Mato Grosso', 'abbreviation' => 'MT']);
+        DB::table('states')->insert(['id' => 12, 'name' => 'Mato Grosso do Sul', 'abbreviation' => 'MS']);
+        DB::table('states')->insert(['id' => 13, 'name' => 'Minas Gerais', 'abbreviation' => 'MG']);
+        DB::table('states')->insert(['id' => 14, 'name' => 'Pará', 'abbreviation' => 'PA']);
+        DB::table('states')->insert(['id' => 15, 'name' => 'Paraíba', 'abbreviation' => 'PB']);
+        DB::table('states')->insert(['id' => 16, 'name' => 'Paraná', 'abbreviation' => 'PR']);
+        DB::table('states')->insert(['id' => 17, 'name' => 'Pernambuco', 'abbreviation' => 'PE']);
+        DB::table('states')->insert(['id' => 18, 'name' => 'Piauí', 'abbreviation' => 'PI']);
+        DB::table('states')->insert(['id' => 19, 'name' => 'Rio de Janeiro', 'abbreviation' => 'RJ']);
+        DB::table('states')->insert(['id' => 20, 'name' => 'Rio Grande do Norte', 'abbreviation' => 'RN']);
+        DB::table('states')->insert(['id' => 21, 'name' => 'Rio Grande do Sul', 'abbreviation' => 'RS']);
+        DB::table('states')->insert(['id' => 22, 'name' => 'Rondônia', 'abbreviation' => 'RO']);
+        DB::table('states')->insert(['id' => 23, 'name' => 'Roraima', 'abbreviation' => 'RR']);
+        DB::table('states')->insert(['id' => 24, 'name' => 'Santa Catarina', 'abbreviation' => 'SC']);
+        DB::table('states')->insert(['id' => 25, 'name' => 'São Paulo', 'abbreviation' => 'SP']);
+        DB::table('states')->insert(['id' => 26, 'name' => 'Sergipe', 'abbreviation' => 'SE']);
+        DB::table('states')->insert(['id' => 27, 'name' => 'Tocantins', 'abbreviation' => 'TO']);
     }
 
 }
