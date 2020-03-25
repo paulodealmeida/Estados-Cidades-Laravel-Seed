08976586e17353aae0dfac7ea0237ec1b83db602
Developer2-Multisistemas <62024604+Developer2-Multisistemas@users.noreply.github.com>
2020-03-25 08:45:09 -0300

    Update 2016_04_26_165108_create_states_table.php

--- 2016_04_26_165108_create_states_table.php
+++ 2016_04_26_165108_create_states_table.php
@@ -14,7 +14,7 @@ class CreateStatesTable extends Migration
     public function up()
     {
         Schema::create('states', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
 
             $table->string('name', 50);
             $table->string('abbreviation', 5);
